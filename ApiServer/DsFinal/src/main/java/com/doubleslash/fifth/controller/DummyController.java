086c13c9e6574936cfb0f9fa9cfb8a18164e2e20
yjsong67 <yj_song67@naver.com>
2020-12-22 01:17:36 +0900

    auth 수정

--- ApiServer/DsFinal/src/main/java/com/doubleslash/fifth/controller/DummyController.java
+++ ApiServer/DsFinal/src/main/java/com/doubleslash/fifth/controller/DummyController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.doubleslash.fifth.auth.AuthService;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -33,16 +36,29 @@ public class DummyController {
 	@ResponseBody
 	public void authTest(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		
-		String uid = (String) request.getAttribute("uid");
+		String idToken = authService.getBearerToken(request);
+		FirebaseToken decodedToken = null;
+		String uid = "";
+		String result = "";
 		
-		if(uid == null) {
+		if(idToken == null) {
+			result = "토큰이 존재하지 않습니다.";
 			response.sendError(401, "Unauthorized");
+			System.out.println(result);
+			return;
 		}
 		
-		System.out.println("idToken : " + uid);
-		
-		
-		
+		try{
+			decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
+		}catch(FirebaseAuthException e) {
+			result = "유효하지 않은 토큰입니다.";
+			response.sendError(401, "Unauthorized");
+			System.out.println(result);
+			return;
+		}
+	
+		uid = decodedToken.getUid();
+
 		
 	}
 	
